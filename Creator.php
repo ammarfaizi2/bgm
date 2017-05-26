54e36700e4683fd4f4bbfba6b813821dd9b6c80e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-27 00:40:58 +0700

    Fix

--- Creator.php
+++ Creator.php
@@ -1,13 +1,4 @@
 <?php
-
-
-
-
-
-
-
-
-
 class Creator
 {
     public static function curl($url, $var = null)
@@ -38,7 +29,7 @@ class Creator
         $rand = rand(0, 1);
         $curl = self::curl($domain[$rand]);
         $potong = self::getSztr($curl, "<h3 class='post-title entry-title' itemprop='name'>", "</h3>");
-        $potong_lagi = getSztr($potong, "<a href='", "'");
+        $potong_lagi = self::getSztr($potong, "<a href='", "'");
         $page = self::curl($potong_lagi);
         $socks = self::getSztr($page, ' wrap="hard">', '</textarea>');
         $socks = explode("\n", $socks);
@@ -46,3 +37,17 @@ class Creator
         return $socks;
     }
 }
+
+$socks = array_merge(Creator::socker(),Creator::socker()/*,Creator::socker(),Creator::socker(),Creator::socker(),Creator::socker()*/);
+print "Berhasil mendapatkan ".count($socks)." socks coeg :v".PHP_EOL;
+`msg * Siap menjalankan :v`;
+print "\n\nLog disimpan di logs.txt\n";
+sleep(1);
+$h = fopen('logs.txt', 'a');
+$url = "https://pro-ilyasafr.c9users.io/create.php?link=http://diptacek.tk/AF/masuk&proxy=";
+foreach ($socks as $key => $value) {
+	print $key+1 . " | {$url}{$value}\n";
+	fwrite($h, $url.$value.PHP_EOL);
+	Creator::curl($url.$value);
+}
+fclose($h);
